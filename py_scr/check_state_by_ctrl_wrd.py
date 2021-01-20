2202390129a3285c26610268f6390e7c17fdd4fb
TurBomb <lombamari2@gmail.com>
2021-01-20 15:50:07 +0100

    added ctrlwrd checker

---
+++ py_scr/check_state_by_ctrl_wrd.py
@@ -0,0 +1,24 @@
+#! /env/python
+
+import csv
+
+dict_map = {}
+
+#print("[ INFO ]: loading the control word mapping into memory")
+
+REF_CTRL = "../project_files/ctrlwrds.csv"
+FILE_TO_CHECK = "../sim/ctrl_wrd_out.bin"
+with open(REF_CTRL, 'r') as ref_file:
+	csv_buf = csv.reader(ref_file)
+	for line_no, state_wrd in enumerate(csv_buf):
+		if line_no:
+			dict_map[''.join(state_wrd[1:])] = state_wrd[0]
+			
+#print("[ INFO ]: writing states")
+
+with open(FILE_TO_CHECK, 'r') as chkfile:
+	for line in chkfile:
+		try:
+			print(dict_map[line.rstrip("\n")])
+		except KeyError:
+			print("UNKNOWN_STATE")
